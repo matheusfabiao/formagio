f12c8952c6c62188113bbaf71453caee5f306a41
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-09-09 14:59:46 -0300

    guest admin config

--- graduation/admin.py
+++ graduation/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Guest
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email')
+    list_filter = ('created_at',)
+
+
+admin.site.register(Guest, GuestAdmin)
