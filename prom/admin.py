a63cdad3cefd55eac08ff5947dc8fdaef87dc7d8
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-11-06 12:38:13 -0300

    admin model registered

--- prom/admin.py
+++ prom/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import PromGuest
+
+
+class PromGuestAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'confirmed_presence')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('confirmed_presence',)
+
+
+admin.site.register(PromGuest, PromGuestAdmin)
